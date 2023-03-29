c623c458a8e5a104b8c0976303d04a104abff87c
mihadcse <huzzatullah@iut-dhaka.edu>
2023-03-30 01:18:01 +0600

    Options and background created

--- BENGAL'S AIR ASSAULT_MAIN.cpp
+++ BENGAL'S AIR ASSAULT_MAIN.cpp
@@ -13,26 +13,52 @@ vector<pair <double,double> >fire;
 
 int main()
 {
-	    int clicked = 0;
-		//WINDOW CREATE AND BACKGROUND
-		sf::RenderWindow window(sf::VideoMode(1000, 600), "Bengal's air assault");
-		sf::Texture backgroundtexture1;
-		if (!backgroundtexture1.loadFromFile("Image/background.jpg"))
-		{
-			cout << "background 1 error!!!!\n";
-		}
-		sf::Sprite backgroundsprite1(backgroundtexture1);
+	int clicked = 0;
+	//WINDOW CREATE AND BACKGROUND
+	sf::RenderWindow window(sf::VideoMode(1000, 600), "Bengal's air assault");
+	sf::Texture backgroundtexture1;
+	if (!backgroundtexture1.loadFromFile("Image/background.jpg"))
+	{
+		cout << "background 1 error!!!!\n";
+	}
+	sf::Sprite backgroundsprite1(backgroundtexture1);
 
-	    //NEW WINDOW CREATE
-		sf::Texture newbackground;
-		if (!newbackground.loadFromFile("Image/b2.jpg"))
-		{
-			std::cout << "NEW background error\n";
-		}
-		sf::Sprite newbackgroundsprite(newbackground);
+	//NEW GAME BACKGROUND
+	sf::Texture newbackground;
+	if (!newbackground.loadFromFile("Image/b2.jpg"))
+	{
+		std::cout << "NEW background error\n";
+	}
+	sf::Sprite newbackgroundsprite(newbackground);
+
+	//HIGH SCORE BACKGROUND
+	sf::Texture scorebackground;
+	if (!scorebackground.loadFromFile("Image/LB1.jpg"))
+	{
+		std::cout << "credits background error\n";
+	}
+	sf::Sprite scorebackgroundsprite(scorebackground);
+
+
+	//KILO FLIGHT BACKGROUND
+
+	//CREDITS BACKGROUND
+	sf::Texture creditbackground;
+	if (!creditbackground.loadFromFile("Image/b4.jpg"))
+	{
+		std::cout << "credits background error\n";
+	}
+	sf::Sprite creditbackgroundsprite(creditbackground);
 
 
 
+
+	//FONT
+	sf::Font font;
+	if (!font.loadFromFile("Fonts/Bootcamp.otf"))
+	{
+		cout << "Font error!!!\n";
+	}
 
 	//BACKGROUND SOUND
 	sf::SoundBuffer backsoundbuffer;
@@ -46,15 +72,91 @@ int main()
 
 
 
-	//OPTIONS CREATE
-	sf::RectangleShape rectangle(sf::Vector2f(100, 50));
-	rectangle.setPosition(200, 100);
-	rectangle.setFillColor(sf::Color::Red);
-	rectangle.setOutlineColor(sf::Color::White);
+	//NEW GAME OPTION CREATE
+	sf::RectangleShape rectangle1(sf::Vector2f(150, 50));
+	rectangle1.setPosition(445, 100);
+	rectangle1.setFillColor(sf::Color::Black);
+	rectangle1.setOutlineColor(sf::Color::Cyan);
+	rectangle1.setOutlineThickness(3);
+	
+	//NEW GAME TEXT
+	sf::Text option1;
+	option1.setFont(font);
+	option1.setString("NEW GAME");
+	option1.setCharacterSize(40);
+	option1.setFillColor(sf::Color::Blue);
+	option1.setPosition(454, 100);
+	
 
-	//NEW OPTION CREATE
+	//HIGH SCORE OPTION CREATE
+	sf::RectangleShape rectangle2(sf::Vector2f(170, 50));
+	rectangle2.setPosition(440, 180);
+	rectangle2.setFillColor(sf::Color::Black);
+	rectangle2.setOutlineColor(sf::Color::Cyan);
+	rectangle2.setOutlineThickness(3);
+
+	//HIGH SCORE TEXT
+	sf::Text option2;
+	option2.setFont(font);
+	option2.setString("HIGH SCORE");
+	option2.setCharacterSize(40);
+	option2.setFillColor(sf::Color::Blue);
+	option2.setPosition(452, 180);
+
+	//CREDITS OPTION CREATE
+	sf::RectangleShape rectangle3(sf::Vector2f(120, 50));
+	rectangle3.setPosition(460, 260);
+	rectangle3.setFillColor(sf::Color::Black);
+	rectangle3.setOutlineColor(sf::Color::Cyan);
+	rectangle3.setOutlineThickness(3);
+
+	//CREDITS SCORE TEXT
+	sf::Text option3;
+	option3.setFont(font);
+	option3.setString("CREDITS");
+	option3.setCharacterSize(40);
+	option3.setFillColor(sf::Color::Blue);
+	option3.setPosition(470, 260);
+
+	//KILO FLIGHT OPTION CREATE
+	sf::RectangleShape rectangle4(sf::Vector2f(170, 50));
+	rectangle4.setPosition(440, 340);
+	rectangle4.setFillColor(sf::Color::Black);
+	rectangle4.setOutlineColor(sf::Color::Cyan);
+	rectangle4.setOutlineThickness(3);
+
+	//KILO FLIGHT TEXT
+	sf::Text option4;
+	option4.setFont(font);
+	option4.setString("KILO FLIGHT");
+	option4.setCharacterSize(40);
+	option4.setFillColor(sf::Color::Blue);
+	option4.setPosition(452, 340);
+
+	//EXIT OPTION CREATE
+	sf::RectangleShape rectangle5(sf::Vector2f(85, 50));
+	rectangle5.setPosition(470, 420);
+	rectangle5.setFillColor(sf::Color::Black);
+	rectangle5.setOutlineColor(sf::Color::Cyan);
+	rectangle5.setOutlineThickness(3);
+
+	//EXIT OPTION TEXT
+	sf::Text option5;
+	option5.setFont(font);
+	option5.setString("EXIT");
+	option5.setCharacterSize(40);
+	option5.setFillColor(sf::Color::Blue);
+	option5.setPosition(485, 420);
 
 
+	//BACK OPTION CREATE
+	sf::RectangleShape rectangle6(sf::Vector2f(40, 40));
+	rectangle6.setPosition(50, 50);
+	rectangle6.setFillColor(sf::Color::Red);
+	rectangle6.setOutlineColor(sf::Color::Cyan);
+	rectangle6.setOutlineThickness(3);
+
+	//MUSIC ON/OFF OPTION
 
 
 
@@ -77,7 +179,7 @@ int main()
 
 	//FIRE SOUND
 	sf::SoundBuffer firesoundbuffer;
-	if (!firesoundbuffer.loadFromFile("Image/fire1.WAV"))
+	if (!firesoundbuffer.loadFromFile("AUDIO/gun1.wav"))
 	{
 		cout << "Fire sound error!!!\n";
 	}
@@ -102,6 +204,17 @@ int main()
 	enemy2sprite.setPosition(sf::Vector2f(500, 50));
 
 
+	//MOUSE CURSOR APPEARANCE
+	sf::Image cursorImage;
+	cursorImage.loadFromFile("Image/cursor1.png");
+
+	// Create a cursor object from the image file
+	sf::Cursor cursor;
+	cursor.loadFromPixels(cursorImage.getPixelsPtr(), cursorImage.getSize(), sf::Vector2u(0, 0));
+
+	// Set the cursor for the window
+	window.setMouseCursor(cursor);
+
 
 
 
@@ -120,22 +233,55 @@ int main()
 			{
 			
 				sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-				sf::FloatRect rectangleBounds = rectangle.getGlobalBounds();
+				sf::FloatRect rectangleBounds = rectangle1.getGlobalBounds();
 				
-				if (rectangle.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+				//NEW GAME CLICKED
+				if (clicked == 0 && rectangle1.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
 				{
-					std::cout << "you clicked!!\n";
+					std::cout << "new game clicked!!\n";
 					 clicked = 1;	
+				}
+				//HIGH SCORE CLICKED
+				else if (clicked==0 && rectangle2.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+				{
+					std::cout << "high score clicked!!\n";
+					clicked = 2;
+				}
+				//CREDITS CLICKED
+				else if (clicked == 0 && rectangle3.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+				{
+					std::cout << "credits clicked!!\n";
+					clicked = 3;
+				}
+
+				//BACK CLICKED
+				else if ((clicked == 2 || clicked==3 || clicked==4) && rectangle6.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+				{
+					std::cout << "back clicked!!\n";
+					clicked = 0;
+				}
+				//KILO FLIGHT CLICKED
+				else if (clicked == 0 && rectangle4.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+				{
+					std::cout << "kilo flight clicked!!\n";
+					clicked = 4;
+				}
+				//EXIT CLICKED
+				else if (clicked == 0 && rectangle5.getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+				{
+					std::cout << "exit clicked!!\n";
+					clicked = 5;
 				}
 			}
 
-
-
-			else if (event.type == sf::Event::MouseButtonReleased && event.key.code == sf::Mouse::Left)
+			if (clicked == 1)
 			{
-				firesound.play();
+				 if (event.type == sf::Event::MouseButtonReleased && event.key.code == sf::Mouse::Left)
+				 {
+					 firesound.play();
 
-				fire.push_back(make_pair(charactersprite.getPosition().x, charactersprite.getPosition().y));
+					 fire.push_back(make_pair(charactersprite.getPosition().x, charactersprite.getPosition().y));
+			     }
 			}
 		}
 
@@ -178,25 +324,50 @@ int main()
 			}
 		
 
-
-
-		
-
-
-
-
-
 			//DRAW AND SHOW IN WINDOW
 			window.clear();
-			if(clicked==0)
-			window.draw(backgroundsprite1);
-			if (clicked == 1)
+			if (clicked == 0)
+			{
+				window.draw(backgroundsprite1);
+				window.draw(rectangle1);
+				window.draw(option1);
+				window.draw(rectangle2);
+				window.draw(option2);
+				window.draw(rectangle3);
+				window.draw(option3);
+				window.draw(rectangle4);
+				window.draw(option4);
+				window.draw(rectangle5);
+				window.draw(option5);
+			}
+
+			else if (clicked == 1)
 			{
 				window.draw(newbackgroundsprite);
-		
+				window.draw(charactersprite);
 			}
-			window.draw(charactersprite);
-			window.draw(rectangle);
+
+			else if (clicked == 2)
+			{
+				window.draw(scorebackgroundsprite);
+				window.draw(rectangle6);
+			}
+
+			else if (clicked == 3)
+			{
+				window.draw(creditbackgroundsprite);
+				window.draw(rectangle6);
+			}
+
+			else if (clicked == 4)
+			{
+				window.draw(rectangle6);
+			}
+
+			else if (clicked == 5)
+			{
+				window.close();
+			}
 				
 			
 			//window.draw(enemy1sprite);
